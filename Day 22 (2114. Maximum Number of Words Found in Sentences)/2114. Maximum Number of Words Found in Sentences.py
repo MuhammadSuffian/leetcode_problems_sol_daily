4b6cff4159dba3c8a57b4d518e5fe4c3f82139a8
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2026-08-10 23:21:05 +0500

    Create 2114. Maximum Number of Words Found in Sentences.py

---
+++ Day 22 (2114. Maximum Number of Words Found in Sentences)/2114. Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,16 @@
+def mostWordsFound():
+    sentences = ["w jrpihe zsyqn l dxchifbxlasaehj","nmmfrwyl jscqyxk a xfibiooix xolyqfdspkliyejsnksfewbjom","xnleojowaxwpyogyrayfgyuzhgtdzrsyococuqexggigtberizdzlyrdsfvryiynhg","krpwiazoulcixkkeyogizvicdkbrsiiuhizhkxdpssynfzuigvcbovm","rgmz rgztiup wqnvbucfqcyjivvoeedyxvjsmtqwpqpxmzdupfyfeewxegrlbjtsjkusyektigr","o lgsbechr lqcgfiat pkqdutzrq iveyv iqzgvyddyoqqmqerbmkxlbtmdtkinlk","hrvh efqvjilibdqxjlpmanmogiossjyxepotezo","qstd zui nbbohtuk","qsdrerdzjvhxjqchvuewevyzlkyydpeeblpc"]
+    conts=[]
+    for a in sentences:
+        count=0
+        for i in a:
+            if i==" ":
+                count+=1
+        count+=1
+        print(count)
+        conts.append(count)
+    return max(conts)
+
+
+
+print("Max number of works: "+str(mostWordsFound()))
