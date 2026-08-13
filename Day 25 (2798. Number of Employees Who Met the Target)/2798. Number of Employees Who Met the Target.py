55ec32a54d4c95d1666a39e91a548beef9e75607
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2026-08-13 17:00:09 +0500

    Create 2798. Number of Employees Who Met the Target.py

---
+++ Day 25 (2798. Number of Employees Who Met the Target)/2798. Number of Employees Who Met the Target.py
@@ -0,0 +1,12 @@
+def numberOfEmployeesWhoMetTarget():
+     hours = [5,1,4,2,2] 
+     target = 6
+     
+     count=0
+     for i in hours:
+          if i>=target:
+               count+=1
+     return count
+
+
+print("Output: "+ str(numberOfEmployeesWhoMetTarget()))
